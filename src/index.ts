88175554dd7b9fa702cc2268aeff5f129dee331b
sandiarbaa <sandiarbaa@gmail.com>
2024-10-12 18:44:12 +0700

    setup express typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
